fb31685d9c3d5872355c9edcd56f97016c167b9b
2Dman <willemriebbels@gmail.com>
2015-11-25 09:27:42 +0100

    v1.02.151125
    v1.02.151125

--- SparkInstall.ps1
+++ SparkInstall.ps1
@@ -1,5 +1,5 @@
 # Script name:   	SparkInstall.ps1
-# Version:			v1.01.151119
+# Version:			v1.02.151125
 # Created on:    	19/11/2015
 # Author:        	Riebbels Willem
 # Purpose:       	Install Spark
@@ -15,37 +15,41 @@
 #	PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public 
 #	License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-#Begin Variable
+#Variable
 $Architecture = $ENV:PROCESSOR_ARCHITECTURE
 $SparkInstallPath = "C:\DRV\Spark\spark_2_7_3_online.exe"
 $SparkKerberos = "HKLM\System\CurrentControlSet\Control\Lsa\Kerberos\Parameters"
 $SparkInstaller = "2.7.3"
-$SparkBackupUserFolder = "C:\Users\$env:username\AppData\Roaming\Spark\Backup"
-
-#Begin Variable Spark properties File
-#------------------------------------------------------------
-$SparkProperties = "$ProgramsFolder\spark.properties"
-# Spark Connection Settings
-$hostAndPort = $true
-$xmppHost = "server.localdomain"
-$xmppPort = 5222
-$ssoMethod = "dns"
-$ssoEnabled = $true
-#Login
-$server = "server.localdomain"
-$autoLoginEnabled = $true
-$username = "$env:username"
-$loginAsInvisibleEnabled = $false
-#Contact
-$showEmptyGroups = $true
-$showOfflineUsers = $true
-$offlineGroupVisible = $false
-$startHidden = $false
-#------------------------------------------------------------
-#End Variable Spark Properties File
+$SparkBackupUserFolder = "C:\Users\$env:username\AppData\Roaming\SparkBackup"
+$CurrentJavaVersion32Bit = (Get-ChildItem "C:\Program Files (x86)\Java\jre*\bin\java.exe").VersionInfo.ProductVersion
+$SparkInstallerArgumentsList  = "-q -console -dir ""$ProgramsFolder"""
 #End Variable
 
+
+if($CurrentJavaVersion32Bit -ge 7.0.51){
+
+#Kill Spark
+stop-Process -name spark
+
+#Start BackupUserChatHistory
+if(Test-Path $SparkBackupUserFolder){}else{new-item $SparkBackupUserFolder -type Directory}
+Write-Host "Backup user History to $SparkBackupUserFolder"
+cp C:\Users\$env:username\AppData\Roaming\SparkBack\user\ $SparkBackupUserFolder
+
+#Spark Uninstall Cleanup
+Write-Host "Spark Uninstall Cleanup"
+Get-ChildItem -Path "C:\Program Files (x86)\Spark\" -Recurse | Remove-Item -force -recurse
+Remove-Item "C:\Program Files (x86)\Spark\" -force
+
+#Spark User uninstall Cleanup
+Write-Host "Spark User uninstall Cleanup"
+Get-ChildItem -Path "C:\Users\$env:username\AppData\Roaming\Spark\" -Recurse | Remove-Item -force -recurse
+Remove-Item "C:\Users\$env:username\AppData\Roaming\Spark\" -force
+
+#Temp Folder cleanup Spark Uses
+Write-Host "Temp Folder cleanup Spark Uses"
+Get-ChildItem -Path "C:\Users\$env:username\AppData\Local\Temp" -Recurse | Remove-Item -force -recurse
+Remove-Item "C:\Users\$env:username\AppData\Local\Temp" -force
 
 if ($Architecture = "AMD64")
 {
@@ -64,23 +68,32 @@ else
 $SparkVersion = $null
 }
 
-$SparkInstallerArgumentsList  = "-q -console -dir ""$ProgramsFolder"""
-#End Variable
-
-#Kill Spark
-stop-Process -name spark
-
 reg add $SparkKerberos /v AllowTGTSessionKey /t REG_DWORD /d 1 /f
 
 if($SparkVersion -lt $SparkInstaller -Or $SparkVersion -eq $null){
 Write-host "Installing Spark client..."
-
-if(Test-Path $SparkBackupUserFolder){}else{new-item $SparkBackupUserFolder -type Directory}
-Write-Host "Backup user History to $SparkBackupUserFolder"
-cp C:\Users\$env:username\AppData\Roaming\Spark\user\$SparkBackupUserFolder
-
 start-process -wait -Filepath $SparkInstallPath -ArgumentList $SparkInstallerArgumentsList
 }
+
+#Begin Variable Spark properties File
+$SparkProperties = "$ProgramsFolder\spark.properties"
+# Spark Connection Settings
+$hostAndPort = $true
+$xmppHost = "genim01.gentgrp.gent.be"
+$xmppPort = 5222
+$ssoMethod = "dns"
+$ssoEnabled = $true
+#Login
+$server = "genim01.gentgrp.gent.be"
+$autoLoginEnabled = $true
+$username = "$env:username"
+$loginAsInvisibleEnabled = $false
+#Contact
+$showEmptyGroups = $true
+$showOfflineUsers = $true
+$offlineGroupVisible = $false
+$startHidden = $false
+#End Variable Spark Properties File
 
 Remove-item $SparkProperties -force | out-null
 New-Item $SparkProperties -type file
@@ -98,12 +111,15 @@ Add-Content $SparkProperties -Value "offlineGroupVisible=$offlineGroupVisible"
 Add-Content $SparkProperties -Value "starthidden=$startHidden"
 Add-Content $SparkProperties -Value "loginAsInvisibleEnabled=$loginAsInvisibleEnabled"
 
-#Personal Spark Settings
+#Peronsal Spark Settings
 $PersonalSparkSettings = "C:\Users\$username\AppData\Roaming\Spark\spark.properties"
 New-Item $PersonalSparkSettings -type file
 Remove-item $PersonalSparkSettings -force | out-null
 
 Add-Content $PersonalSparkSettings -Value "username=$username"
+
+#Restore Spark Chat History
+cp $SparkBackupUserFolder C:\Users\$env:username\AppData\Roaming\SparkBack\user\ 
 
 #Ignite Settings
 Start-process -filepath $sparkexe
@@ -115,3 +131,8 @@ Sleep -s 2
 
 #Start Spark
 Start-process -filepath $sparkexe
+}
+else
+{
+Exit 2
+}
