70291526e9c77e70d7ca9acd895267eed4d313b5
2Dman <willemriebbels@gmail.com>
2015-12-03 13:17:56 +0100

    v1.03.120315
    v1.03.120315

--- SparkInstall.ps1
+++ SparkInstall.ps1
@@ -1,5 +1,5 @@
 # Script name:   	SparkInstall.ps1
-# Version:			v1.02.151125
+# Version:			v1.03.120315
 # Created on:    	19/11/2015
 # Author:        	Riebbels Willem
 # Purpose:       	Install Spark
@@ -8,6 +8,9 @@
 # On Oper-Init.eu   http://blog.oper-init.eu
 # Recent History:       	
 #	19/11/15 => First edit
+#   03/12/15 => Check if java is installed
+#            => If Specified Spark version is installed, do nothing
+#			 => Clean up Spark Program files, Appdata\Spark and Java Cache
 # Copyright:
 #	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
 #	by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed 
@@ -17,16 +20,26 @@
 
 #Variable
 $Architecture = $ENV:PROCESSOR_ARCHITECTURE
-$SparkInstallPath = "C:\DRV\Spark\spark_2_7_3_online.exe"
+$SparkInstallPath = "spark_2_7_3.exe"
 $SparkKerberos = "HKLM\System\CurrentControlSet\Control\Lsa\Kerberos\Parameters"
 $SparkInstaller = "2.7.3"
-$SparkBackupUserFolder = "C:\Users\$env:username\AppData\Roaming\SparkBackup"
-$CurrentJavaVersion32Bit = (Get-ChildItem "C:\Program Files (x86)\Java\jre*\bin\java.exe").VersionInfo.ProductVersion
-$SparkInstallerArgumentsList  = "-q -console -dir ""$ProgramsFolder"""
+$Date = get-date -format yyMMddHMs
+$SparkBackupUserFolder = "C:\Users\$env:username\AppData\Roaming\SparkBackup\$date"
+$CurrentJavaVersion32Bit = (Get-ChildItem "C:\Program Files (x86)\Java\jre*\bin\java.exe")
+
+
+
 #End Variable
 
+$JavaPrerequisite = $false
+$versioninfo =@()
+foreach($c in $CurrentJavaVersion32Bit)
+{
+$versioninfo += ($c.versioninfo.productversion).replace(".","")
+}
+$versioninfo | ?{if($_ -ge "7051"){$JavaPrerequisite = $true}}
 
-if($CurrentJavaVersion32Bit -ge 7.0.51){
+if($JavaPrerequisite -eq $true){
 
 #Kill Spark
 stop-Process -name spark
@@ -34,22 +47,22 @@ stop-Process -name spark
 #Start BackupUserChatHistory
 if(Test-Path $SparkBackupUserFolder){}else{new-item $SparkBackupUserFolder -type Directory}
 Write-Host "Backup user History to $SparkBackupUserFolder"
-cp C:\Users\$env:username\AppData\Roaming\SparkBack\user\ $SparkBackupUserFolder
+cp C:\Users\$env:username\AppData\Roaming\Spark\user\ $SparkBackupUserFolder
 
 #Spark Uninstall Cleanup
 Write-Host "Spark Uninstall Cleanup"
-Get-ChildItem -Path "C:\Program Files (x86)\Spark\" -Recurse | Remove-Item -force -recurse
+Get-ChildItem -Path "C:\Program Files (x86)\Spark\" -Recurse | Remove-Item -force -recurse -ErrorAction SilentlyContinue
 Remove-Item "C:\Program Files (x86)\Spark\" -force
 
 #Spark User uninstall Cleanup
 Write-Host "Spark User uninstall Cleanup"
-Get-ChildItem -Path "C:\Users\$env:username\AppData\Roaming\Spark\" -Recurse | Remove-Item -force -recurse
-Remove-Item "C:\Users\$env:username\AppData\Roaming\Spark\" -force
+Get-ChildItem -Path "C:\Users\$env:username\AppData\Roaming\Spark\" -Recurse | Remove-Item -force -recurse -ErrorAction SilentlyContinue
+Remove-Item "C:\Users\$env:username\AppData\Roaming\Spark\" -force -recurse
 
 #Temp Folder cleanup Spark Uses
 Write-Host "Temp Folder cleanup Spark Uses"
-Get-ChildItem -Path "C:\Users\$env:username\AppData\Local\Temp" -Recurse | Remove-Item -force -recurse
-Remove-Item "C:\Users\$env:username\AppData\Local\Temp" -force
+Get-ChildItem -Path "C:\Users\$env:username\AppData\Local\Temp" -Recurse | Remove-Item -force -recurse -ErrorAction SilentlyContinue
+
 
 if ($Architecture = "AMD64")
 {
@@ -59,6 +72,7 @@ else
 {
 	$ProgramsFolder = "C:\program files\Spark"
 }
+$SparkInstallerArgumentsList  = "-q -console -dir ""$ProgramsFolder"""
 $SparkExe = "$ProgramsFolder\Spark.exe"
 if(Test-Path $SparkExe){
 $SparkVersion = ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($SparkExe)).Productversion
@@ -68,12 +82,11 @@ else
 $SparkVersion = $null
 }
 
-reg add $SparkKerberos /v AllowTGTSessionKey /t REG_DWORD /d 1 /f
-
 if($SparkVersion -lt $SparkInstaller -Or $SparkVersion -eq $null){
+reg add $SparkKerberos /v AllowTGTSessionKey /t REG_DWORD /d 1 /f
 Write-host "Installing Spark client..."
 start-process -wait -Filepath $SparkInstallPath -ArgumentList $SparkInstallerArgumentsList
-}
+
 
 #Begin Variable Spark properties File
 $SparkProperties = "$ProgramsFolder\spark.properties"
@@ -95,7 +108,8 @@ $offlineGroupVisible = $false
 $startHidden = $false
 #End Variable Spark Properties File
 
-Remove-item $SparkProperties -force | out-null
+Remove-item $SparkProperties -force  -ErrorAction SilentlyContinue
+Sleep -s 1
 New-Item $SparkProperties -type file
 
 Add-Content $SparkProperties -Value "hostAndPort=$hostAndPort"
@@ -112,25 +126,29 @@ Add-Content $SparkProperties -Value "starthidden=$startHidden"
 Add-Content $SparkProperties -Value "loginAsInvisibleEnabled=$loginAsInvisibleEnabled"
 
 #Peronsal Spark Settings
-$PersonalSparkSettings = "C:\Users\$username\AppData\Roaming\Spark\spark.properties"
-New-Item $PersonalSparkSettings -type file
-Remove-item $PersonalSparkSettings -force | out-null
-
-Add-Content $PersonalSparkSettings -Value "username=$username"
-
-#Restore Spark Chat History
-cp $SparkBackupUserFolder C:\Users\$env:username\AppData\Roaming\SparkBack\user\ 
-
 #Ignite Settings
 Start-process -filepath $sparkexe
 Sleep -s 1
 
 #Kill Spark Again
 stop-Process -name spark
-Sleep -s 2
+Sleep -s 1
+
+$PersonalSparkSettings = "C:\Users\$username\AppData\Roaming\Spark\spark.properties"
+Remove-item $PersonalSparkSettings -force  -ErrorAction SilentlyContinue
+Sleep -s 1
+New-Item $PersonalSparkSettings -type file
+
+Add-Content $PersonalSparkSettings -Value "username=$username"
+
+#Restore Spark Chat History
+cp $SparkBackupUserFolder C:\Users\$env:username\AppData\Roaming\Spark\user\ 
+
+
 
 #Start Spark
 Start-process -filepath $sparkexe
+}
 }
 else
 {
